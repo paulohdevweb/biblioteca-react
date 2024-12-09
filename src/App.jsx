ac7a931ca26b17418998d2e4002782f8104dc3f3
paulohwebdev <nemeth.paulo@outlook.com>
2024-12-09 17:42:38 -0300

    integrando api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [livros, setLivros] = useState([]);
+
+  async function loadData() {
+    const resposta = await fetch("http://localhost:3000/livros");
+    const dados = await resposta.json();
+    setLivros(dados);
+  }
+
+  async function excluirLivro(id) {
+    await fetch(`http://localhost:3000/livros/${id}`, {
+      method: "DELETE",
+    });
+
+    loadData();
+  }
+
+  async function editarLivro(id) {
+    const titulo = window.prompt("Digite um novo titulo:");
+    await fetch(`http://localhost:3000/livros/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ titulo }),
+    });
+    loadData();
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Livros</h1>
+
+      <table>
+        <tbody>
+          {livros.map((livro) => (
+            <tr key={livro.id}>
+              <td>{livro.titulo}</td>
+              <td>{livro.autor}</td>
+              <td>{livro.paginas}</td>
+              <td>{livro.categoria}</td>
+              <td>
+                <button onClick={() => editarLivro(livro.id)}>Editar</button>
+              </td>
+              <td>
+                <button onClick={() => excluirLivro(livro.id)}>Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
